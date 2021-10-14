60bb5bf1322c178a9ac834c7823b310f48c4cb70
yashkhandelwal05 <68495722+yashkhandelwal05@users.noreply.github.com>
2021-10-14 07:51:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index"); // index refers to index.ejs
+});
+
+app.listen(3000, () => {
+  console.log("server started on port 3000");
+});
